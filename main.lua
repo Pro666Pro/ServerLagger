13d75a00c185f93788c93c18e8e4d4f2813d9341
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-28 22:48:56 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local GameName = "Server Killer 💢💥 - By Nexer1234"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "Server Lagger/Killer 💢💥", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "Server Killer 💢💥", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
