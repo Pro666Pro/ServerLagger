0c48490d55c459483e2a21b838698ad0d624556c
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-28 22:38:47 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ for _,v in pairs(game.Players:GetChildren()) do
 local GameName = "Server Killer - By Nexer1234"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "Server Lagger haha", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "Server Lagger/Killer", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
@@ -80,7 +80,6 @@ for i,v in pairs(game.Workspace:GetChildren()) do
                         v:Destroy()
                     end
                 end
-task.wait()
 end
 	 end
 })
